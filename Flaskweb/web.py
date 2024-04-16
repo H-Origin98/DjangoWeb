0807bc4ef48de55dcdb95d077e50a26b4237693f
H-Origin98 <2033027214@qq.com>
2024-04-16 21:50:06 +0800

    完成 templates 的创建

--- Flaskweb/web.py
+++ Flaskweb/web.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')  # 1. 创建了网站和函数index的对应关系
 def index():
-    return "Hello World!"
+    return render_template("index.html") #默认读取 templates 文件中的html 格式数据
 
 
 if __name__ == '__main__':
