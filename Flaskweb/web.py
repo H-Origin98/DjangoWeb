473c76b74b8e75c114d32b84a504b717c4c503cf
Origin <2033027214@qq.com>
2024-04-18 18:50:30 +0800

    完成V0

--- Flaskweb/web.py
+++ Flaskweb/web.py
@@ -6,7 +6,22 @@ app = Flask(__name__)
 @app.route('/')  # 1. 创建了网站和函数index的对应关系
 def index():
     return render_template("index.html") #默认读取 templates 文件中的html 格式数据
+def get_news():
+    return render_template("get_news.html")
+
+"""
+html格式的标签分类：
+    1.行内标签
+        <div></div>
+        <h1>hello world</h1>
+    2.块级标签
+    
+    
+
+
+"""
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
